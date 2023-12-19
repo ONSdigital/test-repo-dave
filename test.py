43c55cb1b263b642809d5c9ece074e1d21e8a957
David Samuel <31853112+daveS2@users.noreply.github.com>
2023-12-19 12:48:49 +0000

    dave

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
-import pillow
+import pickle
 
 def test():
     print("lol")
     test = "dave"
     if test == "dave":
-        return true
+        return True
     elif test == "dave":
-        return true
+        return True
